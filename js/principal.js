e538fbe6710d57b23639b4388356922d9f0e9558
Axel <adalvarez@scl.edu.gt>
2023-06-03 20:18:22 -0600

    Inicio

---
+++ js/principal.js
@@ -0,0 +1,17 @@
+function inicio(){
+    let componentes =
+    `
+    <button>
+       <a href="./index2.html">FunkoWeb</a>
+    </button>
+    
+   
+    `;
+
+  let inicio_de_tienda = document.querySelector(".caja-pa");
+
+  inicio_de_tienda.innerHTML = componentes;
+
+}
+
+inicio();
