b8f93db90125efa487ed95bbfb14ceabd4ec4a66
Axel <adalvarez@scl.edu.gt>
2023-06-28 16:45:40 -0600

    Vuelta a empezar

--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,8 @@
 function primera_parte(){
     let elementos =
-    `<h1 class="titulo">FunkiLand</h1>
-      <button>
-       <a href="./index1.html">FunkoWeb</a>
-      </button>
-      <div class="animado">
-      <img src="./img/XOsX.gif" alt="">
-      </div>
-      `;
+    ``;
 
-  let inicio_de_tienda = document.querySelector(".caja-pa");
+  let inicio_de_tienda = document.querySelector("contenedor");
 
   inicio_de_tienda.innerHTML = elementos;
 
